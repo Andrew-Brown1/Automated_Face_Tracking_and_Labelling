385769a3a07bb396f9d50dc63239ab252811211b
Andrew <ae.brown@mac.com>
2022-07-06 12:25:13 +0100

    download_demo

---
+++ utils/download_demo_data.sh
@@ -0,0 +1,8 @@
+wget -P ../weights_and_data/ https://www.robots.ox.ac.uk/~abrown/VFA_data/weights_and_data/face_images.zip
+wget -P ../weights_and_data/ https://www.robots.ox.ac.uk/~abrown/VFA_data/weights_and_data/videos.zip 
+
+unzip ../weights_and_data/face_images.zip -d ../weights_and_data/
+unzip ../weights_and_data/videos.zip -d ../weights_and_data/
+
+rm ../weights_and_data/face_images.zip
+rm ../weights_and_data/videos.zip
